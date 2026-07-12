fd90350f9f6389fd10f1ec0560f616109e1d9dca
Nils Wrenger <nils@wrenger.net>
2026-07-12 22:21:11 +0200

    :sparkles: Added reload and config commands && Improved config validation && Refactor

--- neoforge/src/main/java/io/github/nwrenger/dynamicperformance/DynamicPerformance.java
+++ neoforge/src/main/java/io/github/nwrenger/dynamicperformance/DynamicPerformance.java
@@ -5,6 +5,7 @@ import net.neoforged.fml.common.Mod;
 
 @Mod(Constants.MOD_ID)
 public class DynamicPerformance {
+
     public DynamicPerformance(IEventBus eventBus) {
         Common.init();
     }
